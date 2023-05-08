832a4b029d5fb23366bebcd366e6e7362d28c12e
tejaspedge <tejaspedge512@gmail.com>
2023-05-08 14:48:46 +0530

    Project done

--- cure-connect/src/App.js
+++ cure-connect/src/App.js
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
 import './App.css';
 import { useSelector } from 'react-redux';
 import { Box } from '@chakra-ui/react';
 import Navbar from './Components/Navbar/Navbar';
 import AllRoutes from './Routes/Allroute.routes';
-=======
-import "./App.css";
-import { useSelector } from "react-redux";
-import { Box } from "@chakra-ui/react";
-import Navbar from "./Components/Navbar/Navbar";
-import AllRoutes from "./Routes/Allroute.routes";
->>>>>>> c7e2a266d4ba13f3cb106b7debb9bd92ead39839
 
 function App() {
   useSelector((state) => console.log(state));
   return (
     <Box>
-<<<<<<< HEAD
       <Navbar /> 
-=======
-      <Navbar />
->>>>>>> c7e2a266d4ba13f3cb106b7debb9bd92ead39839
       <AllRoutes />
     </Box>
   );
